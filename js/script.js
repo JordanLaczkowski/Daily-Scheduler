6b397755efc8e24194f360f27401658c89dd82e0
Jordan <jordan.laczkowski23@gmail.com>
2022-08-15 12:17:50 -0600

    Made it so that each save button worked and saved information to local storage. Also made the layout look better and more evenly spaced. Lastly, I made sure that I had some items saved and that the time was working properly

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ console.log(currentHour);
 
 function timeCompare() {
   $(".time-block").each(function () {
-    //referencing this time-block -- depending on time of day its a certain color
+    //referencing this time-block -- depending on time of day it is a certain color
     var hourBlock = $(this).attr("id");
     if (hourBlock < currentHour) {
       $(this).addClass("past");
@@ -24,6 +24,17 @@ function timeCompare() {
   });
 }
 timeCompare();
+
+$("#9 textarea").val(localStorage.getItem("9"));
+$("#10 textarea").val(localStorage.getItem("10"));
+$("#11 textarea").val(localStorage.getItem("11"));
+$("#12 textarea").val(localStorage.getItem("12"));
+$("#13 textarea").val(localStorage.getItem("13"));
+$("#14 textarea").val(localStorage.getItem("14"));
+$("#15 textarea").val(localStorage.getItem("15"));
+$("#16 textarea").val(localStorage.getItem("16"));
+$("#17 textarea").val(localStorage.getItem("17"));
+
 // for (i = 8; i < 18; i++) {
 //   console.log("entering for loop");
 //   if (i < currentHour) {
@@ -68,6 +79,12 @@ timeCompare();
 //   }
 // }
 
+$(".saveBtn").on("click", function () {
+  var timeKey = $(this).parent().attr("id");
+  var textValue = $(this).siblings("textarea").val();
+  console.log(timeKey, textValue);
+  localStorage.setItem(timeKey, textValue);
+});
 function saveClicked() {
   console.log("save has been clicked");
 }
